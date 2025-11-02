124c813df04221e8d3765cf438f71c2405cbd8a3
Joseph Nechleba <jnech16@gmail.com>
2025-11-02 13:20:51 -0500

    revert api change

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:8080/api",
+// });
